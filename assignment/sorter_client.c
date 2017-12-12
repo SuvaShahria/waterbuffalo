31382b53c0a200b587ac73f733219c2f684c8318
Dhruv Bhardwaj <DhruvB08@Gmail.com>
2017-12-12 17:13:18 -0500

    made some updates to client

--- assignment/sorter_client.c
+++ assignment/sorter_client.c
@@ -14,47 +14,68 @@
 #include <sys/types.h>
 
 
+//doing this in place of a header file for now
+void goThroughDir(char dirName[]);
+int isCSV(char filename[]);
+int okayDir(char filename[]);
+
+
+
 #define PORT 9000
 #define ADDR 127.0.0.1
 
-int main(int argc, char **argv)
-{
+char outputDirName[1000];
+char sortColumn[1000];
+
+int main(int argc, char *argv[]) {
 	
 	int sockfd;
-
 	int port;
-	
 	struct sockaddr_in address;
-	// We need a hostent struct pointer to 
-	// hold server info from gethostbyname()
-	struct hostent *server;
+	struct hostent *server;			// We need a hostent struct pointer to hold server info from gethostbyname()
+	char buffer[256];				// We need a buffer for messages
+	char readDirName[1000];
 
-	// We need a buffer for messages
-	char buffer[256];
-
-	if (argc < 3)
-	{
-		fprintf(stderr, "usage %s hostname portnum\n", argv[0]);
-		exit(0);
+	//checking arguments
+	if (argc < 7) {
+		printf("invalid arguments to run\n");
+		return 0;
 	}
 
-	// initialize portnum variable with argv[2]
-	// you can use atoi for this demo but atoi 
-	// is easy to fail, not a good choice in general
-	port = atoi(argv[2]);
+	strcpy(sortColumn, argv[2]);
+	port = atoi(argv[6]);
+
+	if (argc == 9) {
+		if (strcmp(argv[7], "-d") == 0) {
+			strcpy(readDirName, argv[8]);
+		}
+		else if (strcmp(argv[7], "-o") == 0) {
+			strcpy(outputDirName, argv[8]);
+		}
+	}
+
+	if (argc == 11) {
+		if (strcmp(argv[7], "-d") == 0) {
+			strcpy(readDirName, argv[8]);
+			strcpy(outputDirName, argv[10]);
+		}
+		else if (strcmp(argv[7], "-o") == 0) {
+			strcpy(outputDirName, argv[8]);
+			strcpy(readDirName, argv[10]);
+		}
+	}
+
 	// initialize hostent pointer by calling 
 	// gethostbyname(argv[1])
 	
-
-/*server = gethostbyname(argv[1]);
+	/*
+	server = gethostbyname(argv[1]);
 	// replace hostent pointer with your pointer name
-	if (server == NULL)
-	{
+	if (server == NULL) {
 		// you can also call h_error() instead
 		fprintf(stderr, "ERROR, NO SUCH HOST\n");
 		exit(0);
 	}
-
 	*/
 
 	// Now we need to initialize sockaddr_in struct
@@ -71,52 +92,43 @@ int main(int argc, char **argv)
 	// we do binary copy to initialize sockaddr_in address part
 	// from hostent pointer
 
-	/*bcopy((char*)server->h_addr,
-			(char*)&address.sin_addr.s_addr,
-			server->h_length);
-					*/
+	/*
+	bcopy((char*)server->h_addr, (char*)&address.sin_addr.s_addr, server->h_length);
+	*/
 
 	/****** STEP 1: Create Socket ******/
 	// call socket(AF_INET, SOCK_STREAM, 0) to create socket	
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0)
-	{
-		//perror() report error message
+	if (sockfd < 0) {
 		perror("sock");
-		//exit your program
 		exit(EXIT_FAILURE);
 	}
 
 	/****** STEP 2: Connection *******/
 	// call connect(socketfd, (struct sockaddr*)&..., sizeof(...))
-	if (connect(sockfd, (struct sockaddr*)&address, sizeof(address)) < 0)
-	{
-		// perror() report error message
+	if (connect(sockfd, (struct sockaddr*)&address, sizeof(address)) < 0) {
 		perror("connect");
-		// close socket
 		close(sockfd);
-		// exit your program
 		exit(EXIT_FAILURE);
 	}
-	printf("Please enter your message: ");
+
 	// You can use fgets() in this demo to receive user input
+	printf("Please enter your message: ");
 	fgets(buffer, 256, stdin);
+	
 	// call write to send data
 	int n = write(sockfd, buffer, 256);
-	// check if message is sent successfully
 	
-	if (n <= 0)
-	{
-		// perror() again...
+	// check if message is sent successfully
+	if (n <= 0) {
 		perror("write");
 	}
 
 	// call read to receive data from server
-	/*memset(buffer, 0, 256);
+	/*
+	memset(buffer, 0, 256);
 	n = read(sockfd, buffer, 256);
-	if (n < 0)
-	{
-		// you know what to do
+	if (n < 0) {
 		perror("read");
 	}
 
@@ -124,6 +136,90 @@ int main(int argc, char **argv)
 	
 	/* clean up: don't forget to close socket */
 	close(sockfd);
+	
+	return 0;
+}
+
+/*
+Function to traverse through a directory, and all subdirectories.
+Each new CSV found, a thread is created and operation goes to the thread.
+Each new subdirectory found is stored in an array, and recursively 
+called upon at the end of the function.
+
+params: dirName 	name of directory to traverse
+return: void
+*/
+void goThroughDir(char dirName[]) {
+	DIR *dir;
+	struct dirent *entry;
+	char newDirs[256][750];
+	int numDirs = 0;
+
+	if ((dir = opendir(dirName)) != NULL) {
+		while ((entr = readdir(dir)) != NULL) {
+			char entryName[256];
+			strcpy(entryName, entry->d_name);
+
+			if (isCSV(entryName)) {
+				//going to this part in a bit
+				//supposed to create thread, start function
+				//to send whole CSV to server
+			}
+
+			else if (okayDir(entryName)) {
+				strcat(newDirs[numDirs], dirName);
+				strcat(newDirs[numDirs], "/");
+				strcat(newDirs[numDirs], entryName);
+				numDirs++;
+			}
+		}
+
+		closedir(dir);
+	}
+
+	int i;
+	for (i = 0; i < numDirs; i++) {
+		goThroughDir(newDirs[i]);
+	}
+}
+
+/*
+Function to check if a given file is a CSV.
+
+params: filename	name of the file
+return:	1 if the file is a CSV
+		0 if the file isn't a CSV
+*/
+int isCSV(char filename[]) {
+	int l = strlen(filename);
+
+	if (l < 4) {
+		return 0;
+	}
+
+	if (filename[l - 1] == 'v' && filename[l - 2] == 's' && filename[l - 3] == 'c') {
+		return 1;
+	}
 
 	return 0;
 }
+
+/*
+Function to check if a given string is a directory.
+
+params:	filename	name of possible directory
+return:	0 if it's a bad directory to traverse
+		1 if it's okay
+*/
+int okayDir(char filename[]) {
+	if (filename[0] == '.' && filename[1] == '.') {
+		return 0;
+	}
+
+	if (filename[0] == '.') {
+		return 0;
+	}
+
+	return 1;
+}
+
